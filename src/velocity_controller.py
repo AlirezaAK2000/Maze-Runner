062dd44b72b37ab35df772f8ba4d8d97148e1a65
m4hdin4 <mahdinasri127@gmail.com>
2021-07-19 21:18:13 +0430

    bogs fixed 1

--- src/velocity_controller.py
+++ src/velocity_controller.py
@@ -126,7 +126,7 @@ class RobotController():
         
         N, _ = window_map.shape
         mid_map = (N - 1) / 2
-        distances = np.zeros(map_size)
+        distances = np.zeros((N, N))
         d_max = mid_map * (2 ** 0.5)
         
         # << A - B * d_max = 0 >> and << A + B = M >>.
@@ -134,8 +134,8 @@ class RobotController():
         A = (M * d_max) / (1 + d_max)
         B = M / (1 + d_max)
         
-        for i in range(map_size):
-            for j in range(map_size):
+        for i in range(N):
+            for j in range(N):
                 distances[i,j] = ((i - mid_map) ** 2 + (j - mid_map) ** 2) ** 0.5
         
         magnitude = (window_map ** 2) * (A - B * distances)
