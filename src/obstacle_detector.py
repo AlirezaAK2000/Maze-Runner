8c145e656136c93546ef3971582886cb49f34c66
AlirezaAK2000 <a.akhoundi79@yahoo.com>
2021-07-22 18:48:57 +0430

    last commit

--- src/obstacle_detector.py
+++ src/obstacle_detector.py
@@ -18,8 +18,8 @@ import tf
 class ObstacleDetector:
     def __init__(self):
         
-        self.GRIDSIZE = 235
-        self.WINDOW = 33
+        self.GRIDSIZE = 135
+        self.WINDOW = 21
         self.unit = 0.2
         
         self.ID = 0
@@ -31,19 +31,21 @@ class ObstacleDetector:
         print(rospy.get_rostime())
         # Subscribe to topic /odom published by the robot base
         self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.callback_laser_scan)
-        self.window_pub = rospy.Publisher("/window_data", OccupancyGrid, queue_size=1)
+        self.window_pub = rospy.Publisher("/window_data", OccupancyGrid, queue_size=10)
         self.grid_pub = rospy.Publisher("grid_data", OccupancyGrid, queue_size=10)
         self.laser_flag = False
         
-        self.obstacle_window = 6
+        self.obstacle_window = 8
 
-        self.rate = rospy.Rate(1)
+        self.rate = rospy.Rate(20)
         
         self.base_frame = '/base_link'
         # The odom frame is usually just /odom
         self.odom_frame = '/odom'
         # Initialize the tf listener
         self.tf_listener = tf.TransformListener()
+        
+        self.dist_dec = 1
         
         rospy.sleep(2)
         
@@ -107,31 +109,34 @@ class ObstacleDetector:
                 y = int((range_scan * math.sin(angle) + position["y"]) / self.unit) + int((self.GRIDSIZE - 1) / 2)
                 self.map_numpy[y,x] = min(100, self.map_numpy[y,x] + 1)
         
-        robot_x = int(position["x"] / self.unit) + int((self.GRIDSIZE - 1) / 2)
-        robot_y = int(position["y"] / self.unit) + int((self.GRIDSIZE - 1) / 2)
-        
-        half_window = int((self.WINDOW - 1) / 2)
-        map_window_data = self.map_numpy[robot_y - half_window :robot_y + half_window + 1,robot_x - half_window :robot_x + half_window + 1]
-        
         self.ID += 1
-        
-        map_window = OccupancyGrid()
-        # header
-        map_window.header.seq = self.ID
-        map_window.header.frame_id = "map"
-        # info
-        map_window.info.map_load_time = rospy.get_rostime()
-        map_window.info.resolution = self.unit
-        map_window.info.width = self.WINDOW
-        map_window.info.height = self.WINDOW
-        map_window.info.origin = self.grid_to_pose(robot_x - half_window, robot_y - half_window)
-        # data
-        map_window.data = map_window_data.reshape(-1).tolist()
-        obstacle_detected = len([i for i in self.laser_scan.ranges[:self.obstacle_window] + self.laser_scan.ranges[-self.obstacle_window:] if (not math.isinf(i)) and i < 1]) > 0
-        # print(obstacle_detected)
+        # if self.ID % 10 != 0:
+        obstacle_detected = len([i for i in self.laser_scan.ranges[:self.obstacle_window] + self.laser_scan.ranges[-self.obstacle_window:] if (not math.isinf(i)) and i < self.dist_dec]) > 0
         if obstacle_detected: 
+            robot_x = int(position["x"] / self.unit) + int((self.GRIDSIZE - 1) / 2)
+            robot_y = int(position["y"] / self.unit) + int((self.GRIDSIZE - 1) / 2)
+            
+            half_window = int((self.WINDOW - 1) / 2)
+            map_window_data = self.map_numpy[robot_y - half_window :robot_y + half_window + 1,robot_x - half_window :robot_x + half_window + 1]
+            # print(f"map windo shape : {map_window_data.shape}")
+            
+            
+            map_window = OccupancyGrid()
+            # header
+            map_window.header.seq = self.ID
+            map_window.header.frame_id = "map"
+            # info
+            map_window.info.map_load_time = rospy.get_rostime()
+            map_window.info.resolution = self.unit
+            map_window.info.width = self.WINDOW
+            map_window.info.height = self.WINDOW
+            map_window.info.origin = self.grid_to_pose(robot_x - half_window, robot_y - half_window)
+            # data
+            map_window.data = map_window_data.reshape(-1).tolist()
+            # assert 0 != len(map_window.data)
+            # print(obstacle_detected)
             self.window_pub.publish(map_window)
-        
+            
         map_grid = OccupancyGrid()
         # header
         map_grid.header.seq = self.ID
@@ -145,7 +150,7 @@ class ObstacleDetector:
         # data
         map_grid.data = self.map_numpy.reshape(-1).tolist()
         self.grid_pub.publish(map_grid)
-        
+            
         self.laser_flag = False
 
 
--- src/velocity_controller.py
+++ src/velocity_controller.py
@@ -25,6 +25,9 @@ import sys
 
 import threading
 
+import random
+
+import datetime
 
 ROTATION , MOVE = range(2)
 
@@ -62,7 +65,9 @@ class RobotController():
         rospy.sleep(2)
 
         self.angular_speed = 0.3
-        self.linear_speed = 0.2
+        self.linear_speed = 0.25
+        
+        self.start_time = datetime.datetime.now()
         
         # Find out if the robot uses /base_link or /base_footprint
         try:
@@ -81,31 +86,30 @@ class RobotController():
                 
         self.grid_sub = rospy.Subscriber('/window_data' , OccupancyGrid , self.callback_grid , queue_size=1)
         
-        self.angle_increment = 0.0872665246327718
+        self.angle_increment = 0.01745330492655436 * 5
         
         self.angle_max = 6.28318977355957
         
         self.number_of_sectors = int(self.angle_max/self.angle_increment) + 1
         
-        self.smooth_factor = 5
+        self.smooth_factor = 10
         
         self.smoothing_factors = list(range(1,self.smooth_factor)) + [self.smooth_factor] + list(reversed(range(1,self.smooth_factor))) 
         
-        self.target_point = (0,0)
+        self.target_point = (-9,-6)
         
         
-        self.targets = [(4,0),(-9,-6)]
         
         self.state = MOVE
         self.end_rotation = rospy.get_rostime()
         
-        self.smax = 6
+        self.smax = 12
         
         self.angular_tolerance = 0.2
         
         #TODO
         # tune the funcking threshold
-        self.threshold = 750
+        self.threshold = 8000
         
         tw_msg = Twist()
         
@@ -113,7 +117,7 @@ class RobotController():
         
         self.cmd_vel.publish(tw_msg)
         
-        threading.Thread(target = self.goal_handler).start()
+        # threading.Thread(target = self.goal_handler).start()
         
         
     def goal_handler(self):
@@ -121,9 +125,11 @@ class RobotController():
         while True:
             pos , _  = self.get_odom()
             if math.sqrt((self.target_point[0] - pos.x)**2 + (self.target_point[1] - pos.y)** 2) < 2:
-                self.targets.append(self.targets.pop(0))
-                self.target_point = self.targets[0]
-                print("new target {}".format(self.target_point))
+                print('target reached !!!')
+                print(f'elapsed time : {datetime.datetime.now()-self.start_time}')
+                self.grid_sub.shutdown()
+                msg = Twist()
+                self.cmd_vel.publish(msg)
             sleep(1)
 
     def quat_to_angle(self, quat):
@@ -147,7 +153,7 @@ class RobotController():
     def calculate_magnitude(self, window_map):
         # we have two parameters A , B. 
         # assume << A + B = M>> to reduce the tunable parameters and calculations
-        M = 0.1 # could be any real positive number
+        M = 1 # could be any real positive number
         
         N, _ = window_map.shape
         mid_map = (N - 1) / 2
@@ -216,105 +222,149 @@ class RobotController():
             val /= 2 * (smooth_mid+1) + 1
             histogram[i] = val
             
-        # histogram = histogram[::-1]
-        # nearest valley
-        # print(histogram.tolist())
-        
-        # print(valleys)
-        # print(min(histogram))
-        
-        # print(valleys)
-        
-        
-        # goal = np.arctan2((self.target_point[1] - pos.y),(self.target_point[0] - pos.x))
-        
-        # print("goal : {}".format(goal))
         pos , rotation = self.get_odom()
-        
-        ktarget = self.scale_angle(np.arctan2((self.target_point[1] - pos.y),(self.target_point[0] - pos.x)))
-        # ktarget
-        
-        robot_heading_sector = self.scale_angle(rotation) // self.angle_increment
-        
-        ktarget =int(ktarget / self.angle_increment)
+        robot_heading_sector = int(self.scale_angle(rotation) / self.angle_increment)
         # ktarget =int(ktarget / self.angle_increment)
+        ktarget =len(histogram) + robot_heading_sector
         
         i = ktarget - 1
         j = ktarget + 1
         
-        goal_sector = -1
+        goal_sector = None
         
-        kn,kf = -1,-1
+        kn,kf = None,None
         
-        # histogram = np.array(histogram.tolist() + histogram.tolist() + histogram.tolist())
+        
+        histogram = np.array(histogram.tolist() + histogram.tolist() + histogram.tolist())
+        # histogram = histogram[::-1]
         valleys = histogram <= self.threshold
-        
-        while i >= 0 or j < len(histogram):
-            if i >=0 and valleys[i]:
-                ind = i
-                kn = i
-                valley_size = 1
-                while ind >= 0 and valleys[ind]:
-                    valley_size += 1
-                    ind -= 1
-                if valley_size >= self.smax:
-                    kf = kn - self.smax
-                    print("wide valley")
-                else:
-                    print("tight valley")
-                    kf = ind + 1
+        if not valleys[ktarget]:
+            while i >= 0 or j < len(histogram):
+                res = []
+                if i >=0 and valleys[i]:
+                    ind = i
+                    kn = i
+                    valley_size = 1
+                    while ind >= 0 and valleys[ind]:
+                        valley_size += 1
+                        ind -= 1
+                    if valley_size >= self.smax:
+                        kf = kn - self.smax
+                        print("wide valley")
+                    else:
+                        print("tight valley")
+                        kf = ind + 1
+                        
+                    assert kf is not None
+                    goal_sector = (kf + kn) // 2
+                    print("kf : {} kn : {} ktarg : {} goal_sector : {}".format(
+                        kf , kn , ktarget , goal_sector
+                    ))
+                    res.append((kf , kn , ktarget , goal_sector))
+                i -= 1
+                
+                if j < len(histogram) and valleys[j]:
+                    ind = j
+                    kn = j
+                    valley_size = 1
+                    while ind < len(histogram) and valleys[ind]:
+                        valley_size += 1
+                        ind += 1
+                    if valley_size >= self.smax:
+                        print("wide valley")
+                        kf = kn + self.smax
+                    else:
+                        print("tight valley")
+                        kf = ind - 1
                     
-                assert kf != -1
-                goal_sector = (kf + kn) // 2
-                print("kf : {} kn : {} ktarg : {} goal_sector : {}".format(
-                    kf , kn , ktarget , goal_sector
-                ))
-                break
+                    assert kf is not None
+                    goal_sector = (kf + kn) // 2
+                    print("kf : {} kn : {} ktarg : {} goal_sector : {}".format(
+                        kf , kn , ktarget , goal_sector
+                    ))
+                    res.append((kf , kn , ktarget , goal_sector))
+                    
+                j += 1
+                assert len(res) <= 2
+                if len(res) == 2:
+                    (kf , kn , ktarget , goal_sector) = res[0] if abs(res[0][0] - res[0][1]) > abs(res[1][0] - res[1][1]) else res[1]
+                    break
+                elif len(res) == 1:
+                    (kf , kn , ktarget , goal_sector) = res[0]
+                    break
+                # elif len(res) == 0:
+                    
+                    
+        else:
+            print("target in valley")
+            res = []
+            ind = i
+            kn = i
+            valley_size = 1
+            while ind >= 0 and valleys[ind]:
+                valley_size += 1
+                ind -= 1
+            if valley_size >= self.smax:
+                kf = kn - self.smax
+                print("wide valley")
+            else:
+                print("tight valley")
+                kf = ind + 1
                 
-            i -= 1
+            assert kf is not None
+            goal_sector = (kf + kn) // 2
+            print("kf : {} kn : {} ktarg : {} goal_sector : {}".format(
+                kf , kn , ktarget , goal_sector
+            ))
+            res.append((kf , kn , ktarget , goal_sector))
             
-            if j < len(histogram) and valleys[j]:
-                ind = j
-                kn = j
-                valley_size = 1
-                while ind < len(histogram) and valleys[ind]:
-                    valley_size += 1
-                    ind += 1
-                if valley_size >= self.smax:
-                    print("wide valley")
-                    kf = kn + self.smax
-                else:
-                    print("tight valley")
-                    kf = ind - 1
+            ind = j
+            kn = j
+            valley_size = 1
+            while ind < len(histogram) and valleys[ind]:
+                valley_size += 1
+                ind += 1
+            if valley_size >= self.smax:
+                print("wide valley")
+                kf = kn + self.smax
+            else:
+                print("tight valley")
+                kf = ind - 1
+            
+            assert kf is not None
+            goal_sector = (kf + kn) // 2
+            print("kf : {} kn : {} ktarg : {} goal_sector : {}".format(
+                kf , kn , ktarget , goal_sector
+            ))
+            res.append((kf , kn , ktarget , goal_sector))
+            goal_angles = [self.normalize_angle(g[3] * self.angle_increment) for g in res]
+            print('left valley {} right valley {}'.format(*goal_angles))
+            # (kf , kn , ktarget , goal_sector) = res[0] if abs(abs(goal_angles[0] - rotation)) < abs(abs(goal_angles[1] - rotation)) else res[1] 
+            # (kf , kn , ktarget , goal_sector) = random.choice(res)
+            (kf , kn , ktarget , goal_sector) = res[0] if abs(res[0][0] - res[0][1]) > abs(res[1][0] - res[1][1]) else res[1]
+            
+            
                 
-                assert kf != -1
-                goal_sector = (kf + kn) // 2
-                print("kf : {} kn : {} ktarg : {} goal_sector : {}".format(
-                    kf , kn , ktarget , goal_sector
-                ))
-                break
+        if goal_sector is None:
+            self.threshold = min(histogram) + 1000
+            goal_sector = robot_heading_sector
+            print(f"new threshold : {self.threshold}")
             
-            j += 1
-            
-            
-            
-            
-                
-        assert goal_sector != -1
-            
-        plt.plot(list(range(len(histogram))),histogram)
-        plt.axhline(y=self.threshold , color = 'r')
-        plt.axvline(x=goal_sector , label='goal sector' ,color ='b')
-        plt.axvline(x=kn , label ='kn' , color = 'c')
-        plt.axvline(x=kf , label = 'kf' , color = 'g')
-        plt.axvline(x=robot_heading_sector, label = 'robot heading sector' , color = 'r')
-        plt.axvline(x=ktarget , label = 'ktarget' , color = 'y')
-        
-        plt.plot(list(range(len(histcopy))),histcopy)
-        plt.show()
+       
         goal_angle = self.normalize_angle(goal_sector * self.angle_increment) 
         
         print("rotation : {} goal_angle : {}".format(rotation , goal_angle))
+        # plt.plot(list(range(len(histogram))),histogram)
+        # plt.axhline(y=self.threshold , color = 'r')
+        # plt.axvline(x=goal_sector , label='goal sector' ,color ='b')
+        # if kn is not None and kf is not None:
+        #     plt.axvline(x=kn , label ='kn' , color = 'c')
+        #     plt.axvline(x=kf , label = 'kf' , color = 'g')
+        # # plt.axvline(x=robot_heading_sector + len(histcopy), label = 'robot heading sector' , color = 'r')
+        # plt.axvline(x=ktarget , label = 'ktarget' , color = 'y')
+        
+        # plt.plot(list(range(len(histcopy)*3)), np.array(histcopy.tolist() + histcopy.tolist() + histcopy.tolist()))
+        # plt.show()
         
         tw_msg = Twist()
         
